fb55ab9f28a6b7c677e5678a37baadfc8353a660
nicoaira <aira.nicolas94@gmail.com>
2021-07-18 22:04:27 -0300

    cambios en el GUI

--- GUI/gui.py
+++ GUI/gui.py
@@ -3,12 +3,34 @@ from tkinter import ttk
 from tkinter.messagebox import showinfo
 
 root = tk.Tk()
+root.title('Protocolo - WGene SARS-CoV-2 RT Detection')
+
+
+################## DEFINICION DE FUNCIONES ##################
+
+def seleccion_num_falcons(event):
+
+	num_falcon = int(menu_num_falcon.get())
+	for i, v in enumerate(falcons):
+
+		if i < num_falcon:
+			falcons[i].config(state = 'normal')
+			falcons[i].delete(0,4)
+			falcons[i].insert(0, 44)
+		else:
+			falcons[i].delete(0,4)
+			falcons[i].config(state = 'disabled')
+
+
+################## INICIALIZACION DE FRAMES ##################
 
 
 frame1 = tk.Frame()
 frame2 = tk.Frame()
-frame3 = tk.Frame()
+frame3 = tk.Frame(relief  = tk.GROOVE, borderwidth=3, pady=20)
 sub_frame3 = tk.Frame(master = frame3)
+
+
 
 ################## SELECCION DE REACTIVO ##################
 
@@ -38,8 +60,8 @@ label_num_racks.pack(padx = 10)
 
 
 vlist = list(range(1,10))
-menu_num_racks = ttk.Combobox(master = frame2, values = vlist)
-menu_num_racks.set(9)
+menu_num_racks = ttk.Combobox(master = frame2, values = vlist, state ='readonly', width=3)
+menu_num_racks.set(6)
 
 menu_num_racks.pack(padx = 10, pady = 10)
 
@@ -51,14 +73,12 @@ label_falcons = tk.Label(frame3, text='Falcons a utilizar')
 label_falcons.pack(padx = 10)
 
 
+
 falcon_list = list(range(1,7))
-menu_num_falcon = ttk.Combobox(master = frame3, values = falcon_list)
-menu_num_falcon.set(6)
-
-
+menu_num_falcon = ttk.Combobox(master = frame3, values = falcon_list, state = 'readonly', width =3 )
+menu_num_falcon.bind('<<ComboboxSelected>>', seleccion_num_falcons)
 menu_num_falcon.pack(padx = 10, pady = 10)
 
-# falcons = [volfalcon1, volfalcon2, volfalcon3, volfalcon4, volfalcon5, volfalcon6]
 
 
 label_falcons = tk.Label(frame3, text='Ingrese el volumen en cada Falcon (mL)')
@@ -79,17 +99,18 @@ for i in range(3):
 
 
 
-
 falcons = []
 
 for i in range(3):
 
 	for j in range(1 ,5 ,2):
 
-		volfalcon = tk.Entry(sub_frame3, width = 3)
+		volfalcon = tk.Entry(sub_frame3, width = 4, state = 'disabled')
 		volfalcon.insert(0,'44')
 		falcons.append(volfalcon)
 		volfalcon.grid(row = j, column = i, padx = 10)
+
+
 
 sub_frame3.pack()
 
@@ -101,4 +122,6 @@ frame1.grid(row = 1, column = 1, padx = 10, pady = 10)
 frame2.grid(row = 2, column = 1, padx = 10, pady = 10)
 frame3.grid(row = 3, column = 1, padx = 10, pady = 10)
 
+
+root.resizable(False, False)
 root.mainloop()
