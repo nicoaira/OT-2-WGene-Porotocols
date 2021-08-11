f2781711e226b7141556576fe644df720fda334d
nicoaira <aira.nicolas94@gmail.com>
2021-08-11 20:59:28 -0300

    Cambios en el sistema de tandas y mensajes al operario

--- protocolo-WG.py
+++ protocolo-WG.py
@@ -23,8 +23,10 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 rvo = config.get('REACTIVO', 'reactivo')
 num_racks = int(config.get('NUM_RACKS', 'num_racks'))
+num_tandas = int(config.get('NUM_TANDAS', 'num_tandas'))
 primer_tip = config.get('FIRST_TIP', 'tip')
 last_tube = config.get('LAST_TUBE', 'tube')
+
 
 if rvo == '5x' or rvo == 'nfw':
     vel_asp= int(config.get('VEL_P1000', 'asp'))
@@ -80,14 +82,14 @@ if rvo == '5x' or rvo == 'nfw' or rvo == 'pc':
     altura_base = 12
     altura_limite = 19.1
     rate_mm_mL = 1.86
-    vol_muerto = 200 + vol_dispensar*1.1
+    vol_muerto = vol_pipeta*1.4
 
 elif rvo == '40x':
     vol_limite = 200 # Vol a partir del cual toma desde la base
     altura_base = 2
     altura_limite = 12
     rate_mm_mL = 7.57
-    vol_muerto = vol_dispensar*1.1
+    vol_muerto = vol_pipeta*1.4
 
 
 
@@ -141,7 +143,6 @@ def run(protocol: protocol_api.ProtocolContext):
 
 
     last_tube_obj = racks[-1].wells_by_name()[last_tube]
-    print('ULTIMO TUBO>>>',last_tube_obj)
 
 
     
@@ -157,19 +158,17 @@ def run(protocol: protocol_api.ProtocolContext):
 
 
 
-    tandas = 10
-
-    for t in range(tandas):
+    for t in range(num_tandas):
 
 
         for i in range(num_racks):
 
             c = 0
 
-            wells_per_rack = len(racks[i].wells())  # para el rack nuestro seria 40
+            wells_per_rack = len(racks[i].wells())
+
 
             for falcon, vol in falcons.items():
-
 
                 # Se busca el primer falcon que tenga volumen
                 if vol < vol_muerto:
@@ -180,7 +179,7 @@ def run(protocol: protocol_api.ProtocolContext):
 
 
                 # Configuracion de la altura inicial del tip
-                # Toma como punto limite el cono inferior del falcon (aprox 3750uL)
+                # Toma como punto limite el cono inferior del falcon
                 # A partir de ahi toma desde el fondo
 
                 if vol > vol_limite:
@@ -201,25 +200,29 @@ def run(protocol: protocol_api.ProtocolContext):
 
                         for m in range(vol_pipeta//vol_dispensar):
 
-                            if c < wells_per_rack:
+                            if c < wells_per_rack and vol > vol_muerto:
                                 pipette.dispense(vol_dispensar, racks[i].wells()[c].bottom(7), rate = 0.5)
                                 pipette.touch_tip(racks[i].wells()[c], radius = 0.70, v_offset = -15 )
                                 pipette.default_speed = 80 * factor_vel_mov_ot
                                 protocol.delay(1.2)
 
+                                c_temp = c
+
                                 if racks[i].wells()[c] == last_tube_obj:
-                                    c = wells_per_rack
+                                    c = wells_per_rack  #Para salir del bucle while
                                 else:
                                     c += 1
                             else:
                                 pipette.blow_out(plate[falcon])
                                 break
 
-                        pipette.default_speed = 300 * factor_vel_mov_ot # controla la velocidad general del OT2, predeterminada 400mm/s
+                        pipette.default_speed = 300 * factor_vel_mov_ot 
+                        # controla la velocidad general del OT2, predeterminada 400mm/s
 
                         pipette.dispense(vol_pipeta%vol_dispensar, plate[falcon].bottom(pipette.well_bottom_clearance.aspirate+7), rate = 0.5)
                         protocol.delay(1.2)
                         pipette.blow_out(plate[falcon].bottom(pipette.well_bottom_clearance.aspirate+7))
+
 
 
                         # Volumen utilizado en uL
@@ -228,15 +231,15 @@ def run(protocol: protocol_api.ProtocolContext):
 
                         # Se descuenta el volumen usado del falcon
                         falcons[falcon] -= vol_usado
-                        protocol.comment(str(falcons[falcon]))
+
+                        volumen_actual = 'VOLUMEN EN FALCON ' + falcon + ' > '+ str(falcons[falcon])
+                        protocol.comment(volumen_actual)
 
 
 
                     else:
 
                         pasos = math.ceil(vol_dispensar/vol_pipeta)
-
-
 
 
                         for m in range(pasos):
@@ -251,13 +254,12 @@ def run(protocol: protocol_api.ProtocolContext):
 
 
 
-
                         # Volumen utilizado en uL
                         vol_usado = vol_dispensar*pasos
 
                         # Se descuenta el volumen usado del falcon
                         falcons[falcon] -= vol_usado
-                        print('Volumen en el falcon', falcon, '>>', falcons[falcon])
+
 
                         if racks[i].wells()[c] == last_tube_obj:
                             break
@@ -272,35 +274,58 @@ def run(protocol: protocol_api.ProtocolContext):
                         pipette.well_bottom_clearance.aspirate = .5
 
 
+
                     # Chequeo del volumen del falcon antes de repetir cada paso
                     if falcons[falcon] < vol_muerto:
+
+                        if num_tandas > 1 and racks[i].wells()[c_temp] != last_tube_obj:
+                            protocol.comment('No queda mas volumen para asipirar!')
+                            mensaje_tanda = 'Tanda ' + str(t+1) + '/' + str(num_tandas) + ' incompleta!'
+                            protocol.comment(msg = mensaje_tanda)
+                            protocol.comment(msg = "Chequee cual es el ultimo tubo alicuotado.")
+                            protocol.comment('Terminando protocolo!')
+
+                        elif num_tandas == 1 and racks[i].wells()[c_temp] != last_tube_obj:
+                            protocol.comment(msg = "No se completaron todos lo racks!")
+                            protocol.comment(msg = "Chequee cual es el ultimo tubo alicuotado.")
+                            protocol.comment('Terminando protocolo!')
+
                         break
+
+
+
                     else:
                         continue
 
-        for l in range(5):
-            protocol.set_rail_lights(False)
-            protocol.delay(0.5)
-            protocol.set_rail_lights(True)
-
-
-        if any([True if v > vol_muerto else False for v in falcons.values()]):
-            pass
-
-        else:
-            protocol.comment('No queda mas volumen para asipirar!')
-            protocol.comment('Terminando protocolo!')
-            protocol.cleanup()
-            break
-
-        mensaje_tanda = 'Tanda ' + str(t+1) + '/' + str(tandas) + ' completada!'
-        mensaje_carga = 'Cargue los '+str(num_racks)+' racks nuevos y haga click en "Continue"'
 
 
 
 
-        protocol.comment(msg = mensaje_tanda)
-        protocol.pause(msg = mensaje_carga)
+        if num_tandas > 1:
+
+
+
+
+            if any([True if v > vol_muerto else False for v in falcons.values()]):
+
+                for l in range(5):
+                    protocol.set_rail_lights(False)
+                    protocol.delay(0.5)
+                    protocol.set_rail_lights(True)
+
+                mensaje_tanda = 'Tanda ' + str(t+1) + '/' + str(num_tandas) + ' completada!'
+                protocol.comment(msg = mensaje_tanda)
+
+
+                if t < num_tandas - 1:
+                    mensaje_carga = 'Cargue los '+str(num_racks)+' racks nuevos y haga click en "Resume"'
+                    protocol.pause(msg = mensaje_carga)
+
+            else:
+                protocol.cleanup()
+                break
+
+
 
 
 
